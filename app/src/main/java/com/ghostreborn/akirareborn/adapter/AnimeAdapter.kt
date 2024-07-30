c8cdd1b6e8c3d9ca882a0d20c5bbb5ac707d0b64
TorqueReborn <torquereborn@gmail.com>
2024-07-30 16:26:15 +0530

    Anime Adapter

---
+++ app/src/main/java/com/ghostreborn/akirareborn/adapter/AnimeAdapter.kt
@@ -0,0 +1,32 @@
+package com.ghostreborn.akirareborn.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ghostreborn.akirareborn.Constants
+import com.ghostreborn.akirareborn.R
+
+class AnimeAdapter():RecyclerView.Adapter<AnimeAdapter.AnimeViewHolder>(){
+    class AnimeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val animeImageView = itemView.findViewById<ImageView>(R.id.animeImageView)
+        val animeNameTextView = itemView.findViewById<TextView>(R.id.anime_name_text_view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimeViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.anime_list, parent, false)
+        return AnimeViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return Constants.animeList.size
+    }
+
+    override fun onBindViewHolder(holder: AnimeViewHolder, position: Int) {
+        val anime = Constants.animeList[position]
+        holder.animeNameTextView.text = anime.name
+    }
+
+}
