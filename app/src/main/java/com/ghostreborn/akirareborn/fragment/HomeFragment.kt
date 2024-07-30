6e27d36e4c5cd04a1f56e154954be2e42722fc97
TorqueReborn <torquereborn@gmail.com>
2024-07-30 16:30:47 +0530

    Home Recycler

--- app/src/main/java/com/ghostreborn/akirareborn/fragment/HomeFragment.kt
+++ app/src/main/java/com/ghostreborn/akirareborn/fragment/HomeFragment.kt
@@ -1,19 +1,24 @@
 package com.ghostreborn.akirareborn.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.ghostreborn.akirareborn.R
 
 class HomeFragment : Fragment() {
+
+    private lateinit var homeRecycler: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        homeRecycler = view.findViewById(R.id.home_anime_recycler)
+        return view
     }
 
 }
