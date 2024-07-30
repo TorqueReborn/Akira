750067b29a4bac6d389e62eddea440c74b407817
TorqueReborn <torquereborn@gmail.com>
2024-07-30 19:18:20 +0530

    Store anime ID

--- app/src/main/java/com/ghostreborn/akirareborn/Constants.kt
+++ app/src/main/java/com/ghostreborn/akirareborn/Constants.kt
@@ -4,6 +4,7 @@ import com.ghostreborn.akirareborn.model.Anime
 import com.ghostreborn.akirareborn.model.Episode
 
 object Constants {
+    lateinit var animeID: String
     lateinit var animeThumbnail: String
     lateinit var animeList: ArrayList<Anime>
     lateinit var parsedEpisodes: ArrayList<Episode>
