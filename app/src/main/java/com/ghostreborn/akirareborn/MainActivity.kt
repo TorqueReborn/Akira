2645d19168bfb8361f822dbdd3acb0f8a8579f8b
TorqueReborn <torquereborn@gmail.com>
2024-07-31 05:10:44 +0530

    Anilst Fragment

--- app/src/main/java/com/ghostreborn/akirareborn/MainActivity.kt
+++ app/src/main/java/com/ghostreborn/akirareborn/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     // TODO show progress based on episode titles parsed
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/ghostreborn/akirareborn/fragment/AnilistFragment.kt
@@ -0,0 +1,18 @@
+package com.ghostreborn.akirareborn.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ghostreborn.akirareborn.R
+
+class AnilistFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_anilist, container, false)
+    }
+}
