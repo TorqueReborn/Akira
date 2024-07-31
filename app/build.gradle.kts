3d48f2ef57a54dc88f849333a3a49e1685908127
TorqueReborn <torquereborn@gmail.com>
2024-08-01 04:09:42 +0530

    Room Gradle Implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13"
 }
 
 android {
@@ -40,7 +41,7 @@ android {
 
 dependencies {
     implementation (libs.androidx.room.runtime)
-    annotationProcessor (libs.androidx.room.compiler)
+    ksp(libs.androidx.room.compiler)
     implementation (libs.androidx.room.ktx)
     implementation(libs.picasso)
     implementation(libs.okhttp)
