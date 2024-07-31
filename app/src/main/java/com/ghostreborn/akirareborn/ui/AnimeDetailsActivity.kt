7daccd5436bebae791b716367d88e07047087493
TorqueReborn <torquereborn@gmail.com>
2024-07-31 07:27:18 +0530

    AnimeDetailsActivity -> AnimeDetailsFragment

--- app/src/main/java/com/ghostreborn/akirareborn/ui/AnimeDetailsActivity.kt
+++ app/src/main/java/com/ghostreborn/akirareborn/ui/AnimeDetailsActivity.kt
@@ -1,56 +1,13 @@
 package com.ghostreborn.akirareborn.ui
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.ghostreborn.akirareborn.Constants
-import com.ghostreborn.akirareborn.allanime.AllAnimeParser
-import com.ghostreborn.akirareborn.databinding.ActivityAnimeDetailsBinding
-import com.squareup.picasso.Picasso
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import com.ghostreborn.akirareborn.R
 
 class AnimeDetailsActivity : AppCompatActivity() {
 
-    private lateinit var animeDetailsBinding: ActivityAnimeDetailsBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        animeDetailsBinding = ActivityAnimeDetailsBinding.inflate(layoutInflater)
-        setContentView(animeDetailsBinding.root)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            AllAnimeParser().animeDetails(Constants.anime.id)
-            withContext(Dispatchers.Main) {
-                animeDetailsBinding.animeName.text = Constants.animeDetails.name
-                animeDetailsBinding.animeDescription.text = Constants.animeDetails.description
-                Picasso.get().load(Constants.animeDetails.banner)
-                    .into(animeDetailsBinding.animeBanner)
-                Picasso.get().load(Constants.animeDetails.thumbnail)
-                    .into(animeDetailsBinding.animeThumbnail)
-                if (Constants.animeDetails.prequel.isNotEmpty()) {
-                    animeDetailsBinding.prequelButton.visibility = View.VISIBLE
-                    animeDetailsBinding.prequelButton.setOnClickListener {
-                        Constants.anime.id = Constants.animeDetails.prequel
-                        startActivity(Intent(baseContext, AnimeDetailsActivity::class.java))
-                        finish()
-                    }
-                }
-                if (Constants.animeDetails.sequel.isNotEmpty()) {
-                    animeDetailsBinding.sequelButton.visibility = View.VISIBLE
-                    animeDetailsBinding.sequelButton.setOnClickListener {
-                        Constants.anime.id = Constants.animeDetails.sequel
-                        startActivity(Intent(baseContext, AnimeDetailsActivity::class.java))
-                        finish()
-                    }
-                }
-                animeDetailsBinding.watchFab.setOnClickListener {
-                    startActivity(Intent(baseContext, EpisodeActivity::class.java))
-                }
-            }
-        }
+        setContentView(R.layout.activity_anime_details)
     }
 }
