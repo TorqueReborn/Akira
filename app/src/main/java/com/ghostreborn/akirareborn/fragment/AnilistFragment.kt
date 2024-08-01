2b6c1bf2bbb5a2ef75b3669afc4d0d7a93a48a57
TorqueReborn <torquereborn@gmail.com>
2024-08-02 03:23:02 +0530

    Update algorithm

--- app/src/main/java/com/ghostreborn/akirareborn/fragment/AnilistFragment.kt
+++ app/src/main/java/com/ghostreborn/akirareborn/fragment/AnilistFragment.kt
@@ -21,47 +21,41 @@ import kotlinx.coroutines.withContext
 
 class AnilistFragment : Fragment() {
 
-    lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerView: RecyclerView
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_anilist, container, false)
-        recyclerView = view.findViewById(R.id.anilist_recycler_view)
-        swipeRefreshLayout = view.findViewById(R.id.anilist_swipe_refresh)
-        return view
+    ): View = inflater.inflate(R.layout.fragment_anilist, container, false).apply {
+        recyclerView = findViewById(R.id.anilist_recycler_view)
+        swipeRefreshLayout = findViewById(R.id.anilist_swipe_refresh)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        swipeRefreshLayout.setOnRefreshListener {
-            fetchData()
-        }
+        swipeRefreshLayout.setOnRefreshListener { fetchData() }
         fetchData()
     }
 
-    fun fetchData(){
-        if (!Constants.preferences.getBoolean(Constants.PREF_LOGGED_IN, false)){
-            return
-        }
+    private fun fetchData() {
+        if (!Constants.preferences.getBoolean(Constants.PREF_LOGGED_IN, false)) return
+
         CoroutineScope(Dispatchers.IO).launch {
             val instance = Room.databaseBuilder(
-                requireContext(),
-                AnilistDatabase::class.java,
-                "Akira"
+                requireContext(), AnilistDatabase::class.java, "Akira"
             ).build()
-            val anilists = instance.anilistDao().getAll()
-            var ids = ""
-            for (i in 0 until anilists.size) {
-                ids += "\"" + anilists[i].allAnimeID + "\","
-            }
-            ids = ids.substring(0, ids.length - 1)
+
+            val ids = instance.anilistDao().getAll()
+                .joinToString(separator = ",") { "\"${it.allAnimeID}\"" }
+
             val animes = AllAnimeParser().getDetailsByIds(ids)
+
             withContext(Dispatchers.Main) {
-                recyclerView.adapter = AnimeAdapter(animes)
-                recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
+                recyclerView.apply {
+                    adapter = AnimeAdapter(animes)
+                    layoutManager = GridLayoutManager(requireContext(), 3)
+                }
                 swipeRefreshLayout.isRefreshing = false
             }
         }
