44ce4ec6204029f857bb41ccd6e69fdb666459d5
TorqueReborn <torquereborn@gmail.com>
2024-08-01 21:24:13 +0530

    Show only 2 fragments

--- app/src/main/java/com/ghostreborn/akirareborn/adapter/AnimeViewPagerAdapter.kt
+++ app/src/main/java/com/ghostreborn/akirareborn/adapter/AnimeViewPagerAdapter.kt
@@ -6,18 +6,16 @@ import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.ghostreborn.akirareborn.fragment.AnilistFragment
 import com.ghostreborn.akirareborn.fragment.AnimeFragment
-import com.ghostreborn.akirareborn.fragment.TestFragment
 
-class AnimeViewPagerAdapter (fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+class AnimeViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
 
-    override fun getItemCount(): Int = 3 // Number of tabs
+    override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> AnilistFragment()
             1 -> AnimeFragment()
-            2 -> TestFragment()
             else -> throw IllegalStateException("Invalid tab position")
         }
     }
