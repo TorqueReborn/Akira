72690a9d98b07f1fc960d73ffa8687bf705d215c
TorqueReborn <torquereborn@gmail.com>
2024-08-01 08:31:08 +0530

    Check anime exists in database

---
+++ app/src/main/java/com/ghostreborn/akirareborn/fragment/AnimeDetailsFragment.kt
@@ -0,0 +1,51 @@
+package com.ghostreborn.akirareborn.fragment
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.room.Room
+import com.ghostreborn.akirareborn.database.AnilistDatabase
+import com.ghostreborn.akirareborn.databinding.FragmentAnimeDetailsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class AnimeDetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentAnimeDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAnimeDetailsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkAnilist()
+    }
+
+    private fun checkAnilist() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val db = Room.databaseBuilder(
+                requireContext(),
+                AnilistDatabase::class.java,
+                "Akira"
+            ).build()
+            val anilist = db.anilistDao().findByAllAnimeID(AnimeFragment.allAnimeID)
+            withContext(Dispatchers.Main) {
+                if (anilist != null) {
+                    Log.e("TAG", "mediaID: ${anilist.id}")
+                } else {
+                    Log.e("TAG", "allAnimeID: ${AnimeFragment.allAnimeID}")
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/ghostreborn/akirareborn/ui/AnimeDetailsActivity.kt
+++ app/src/main/java/com/ghostreborn/akirareborn/ui/AnimeDetailsActivity.kt
@@ -3,10 +3,14 @@ package com.ghostreborn.akirareborn.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ghostreborn.akirareborn.R
+import com.ghostreborn.akirareborn.fragment.AnimeDetailsFragment
 
 class AnimeDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_anime_details)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.anime_details_frame_layout, AnimeDetailsFragment())
+            .commit()
     }
 }
