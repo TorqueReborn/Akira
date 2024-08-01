dffbb79987d8d4f2b2a9440cd0f186b762af20e4
TorqueReborn <torquereborn@gmail.com>
2024-08-02 03:07:50 +0530

    Miniaturize

--- app/src/main/java/com/ghostreborn/akirareborn/anilist/AnilistNetwork.kt
+++ app/src/main/java/com/ghostreborn/akirareborn/anilist/AnilistNetwork.kt
@@ -4,50 +4,33 @@ import com.ghostreborn.akirareborn.Constants
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
 
 class AnilistNetwork {
     private fun connectAnilist(graph: String): String? {
-        val token = Constants.preferences.getString(Constants.PREF_TOKEN, "")
-        val client = OkHttpClient()
-        val mediaType = "application/json; charset=utf-8".toMediaTypeOrNull()
-        val json = JSONObject()
-            .put("query", graph)
-            .toString()
-        val body: RequestBody = json.toRequestBody(mediaType)
+        val body = JSONObject().apply {
+            put("query", graph)
+        }.toString().toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
         val request = Request.Builder()
             .url("https://graphql.anilist.co")
             .post(body)
-            .addHeader("Authorization", "Bearer ${token}")
+            .addHeader(
+                "Authorization",
+                "Bearer ${Constants.preferences.getString(Constants.PREF_TOKEN, "")}"
+            )
             .build()
-        val response = client.newCall(request).execute()
-        return response.body?.string()
+        return OkHttpClient().newCall(request).execute().body?.string()
     }
 
     fun saveAnime(animeId: String, status: String, progress: String): String {
-        val query = "mutation{" +
-                "  SaveMediaListEntry(mediaId:$animeId,status:$status, progress:$progress) {" +
-                "    id" +
-                "    media{" +
-                "      idMal" +
-                "      title{" +
-                "        native" +
-                "      }" +
-                "    }" +
-                "    progress" +
-                "  }" +
-                "}"
+        val query =
+            "mutation{SaveMediaListEntry(mediaId:$animeId,status:$status,progress:$progress){id,media{idMal,title{native}},progress}}"
         return connectAnilist(query).toString()
     }
 
     fun deleteAnime(mediaId: String): String {
-        val query = "mutation {" +
-                "  DeleteMediaListEntry(id: $mediaId) {" +
-                "    deleted" +
-                "  }" +
-                "}"
+        val query = "mutation{DeleteMediaListEntry(id:$mediaId){deleted}}"
         return connectAnilist(query).toString()
     }
 }
