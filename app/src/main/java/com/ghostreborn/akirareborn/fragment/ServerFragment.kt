9cba4c72db4e908f45f6e483edee9cf4fb74f33f
TorqueReborn <torquereborn@gmail.com>
2024-08-02 03:26:34 +0530

    Update algorithm

--- app/src/main/java/com/ghostreborn/akirareborn/fragment/ServerFragment.kt
+++ app/src/main/java/com/ghostreborn/akirareborn/fragment/ServerFragment.kt
@@ -22,20 +22,17 @@ class ServerFragment : DialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view =
-            inflater.inflate(R.layout.fragment_server, container, false)
+        val view = inflater.inflate(R.layout.fragment_server, container, false)
 
-        val fragmentRecyclerView: RecyclerView =
-            view.findViewById(R.id.server_recycler_view)
-        fragmentRecyclerView.layoutManager = LinearLayoutManager(context)
-
+        val fragmentRecyclerView: RecyclerView = view.findViewById(R.id.server_recycler_view)
         val progressBar: ProgressBar = view.findViewById(R.id.server_progress_bar)
 
+        fragmentRecyclerView.layoutManager = LinearLayoutManager(context)
+
         CoroutineScope(Dispatchers.IO).launch {
-            val sources =
-                AllAnimeParser().getSourceUrls(AnimeFragment.allAnimeID, AnimeFragment.animeEpisode)
+            val sources = AllAnimeParser().getSourceUrls(AnimeFragment.allAnimeID, AnimeFragment.animeEpisode)
             withContext(Dispatchers.Main) {
-                progressBar.visibility = ProgressBar.GONE
+                progressBar.visibility = View.GONE
                 fragmentRecyclerView.adapter = ServerAdapter(sources)
             }
         }
@@ -45,12 +42,9 @@ class ServerFragment : DialogFragment() {
 
     override fun onStart() {
         super.onStart()
-        val dialog = dialog
-        if (dialog != null) {
-            val width = ViewGroup.LayoutParams.MATCH_PARENT
-            val height = ViewGroup.LayoutParams.WRAP_CONTENT
-            dialog.window!!.setLayout(width, height)
-        }
+        dialog?.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
     }
-
 }
