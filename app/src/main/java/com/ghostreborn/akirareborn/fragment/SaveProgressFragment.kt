a38ab4d9f93404ddfb5dfe76821cc7afece3945b
TorqueReborn <torquereborn@gmail.com>
2024-08-01 05:52:06 +0530

    Save Progress Fragment

---
+++ app/src/main/java/com/ghostreborn/akirareborn/fragment/SaveProgressFragment.kt
@@ -0,0 +1,20 @@
+package com.ghostreborn.akirareborn.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ghostreborn.akirareborn.R
+
+class SaveProgressFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_save_progress, container, false)
+        return view
+    }
+
+}
