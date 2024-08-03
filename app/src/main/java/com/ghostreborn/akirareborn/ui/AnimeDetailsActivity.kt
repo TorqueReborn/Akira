e7517fa4bfacda89c4a09faa60305c0e8f24f8ec
TorqueReborn <torquereborn@gmail.com>
2024-08-03 20:15:28 +0530

    Fragment to Activity

--- app/src/main/java/com/ghostreborn/akirareborn/ui/AnimeDetailsActivity.kt
+++ app/src/main/java/com/ghostreborn/akirareborn/ui/AnimeDetailsActivity.kt
@@ -1,16 +1,73 @@
 package com.ghostreborn.akirareborn.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.ghostreborn.akirareborn.Constants
 import com.ghostreborn.akirareborn.R
-import com.ghostreborn.akirareborn.fragment.AnimeDetailsFragment
+import com.ghostreborn.akirareborn.allAnime.AllAnimeParser
+import com.ghostreborn.akirareborn.databinding.ActivityAnimeDetailsBinding
+import com.ghostreborn.akirareborn.fragment.SaveAnimeFragment
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AnimeDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAnimeDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_anime_details)
+        binding = ActivityAnimeDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         supportFragmentManager.beginTransaction()
-            .replace(R.id.anime_details_frame_layout, AnimeDetailsFragment())
+            .replace(R.id.anilist_frame_layout, SaveAnimeFragment())
             .commit()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val details = AllAnimeParser().animeDetails(Constants.allAnimeID)
+            withContext(Dispatchers.Main) {
+                binding.apply {
+                    animeName.text = details.name
+                    animeDescription.text = details.description
+                    Picasso.get().load(details.banner).into(animeBanner)
+                    Picasso.get().load(details.thumbnail).into(animeThumbnail)
+
+                    binding.animeProgressTextView.text = if (Constants.animeEpisode.isNotEmpty()) {
+                        "Watched ${Constants.animeEpisode} episodes"
+                    } else {
+                        "Not watched"
+                    }
+
+                    watchFab.setOnClickListener {
+                        startActivity(Intent(this@AnimeDetailsActivity, EpisodesActivity::class.java))
+                    }
+
+                    prequelButton.apply {
+                        visibility = if (details.prequel.isNotEmpty()) View.VISIBLE else View.GONE
+                        setOnClickListener {
+                            Constants.allAnimeID = details.prequel
+                            Constants.animeEpisode = ""
+                            startActivity(Intent(context, AnimeDetailsActivity::class.java))
+                        }
+                    }
+
+                    sequelButton.apply {
+                        visibility = if (details.sequel.isNotEmpty()) View.VISIBLE else View.GONE
+                        setOnClickListener {
+                            Constants.allAnimeID = details.sequel
+                            Constants.animeEpisode = ""
+                            startActivity(Intent(context, AnimeDetailsActivity::class.java))
+                        }
+                    }
+                }
+            }
+        }
+
     }
+
 }
