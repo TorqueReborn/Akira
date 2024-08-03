f8ebe1c6b760ac344808cc18284b4bdacbf72725
TorqueReborn <torquereborn@gmail.com>
2024-08-03 22:08:21 +0530

    Fix playback due to progress monitoring

--- app/src/main/java/com/ghostreborn/akirareborn/ui/PlayEpisodeActivity.kt
+++ app/src/main/java/com/ghostreborn/akirareborn/ui/PlayEpisodeActivity.kt
@@ -7,7 +7,7 @@ import android.widget.MediaController
 import android.widget.Toast
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.ghostreborn.akirareborn.Constants
@@ -26,11 +26,13 @@ class PlayEpisodeActivity : AppCompatActivity() {
 
     private lateinit var videoView: VideoView
     private val videoUrl: Uri by lazy { Uri.parse(Constants.animeUrl) }
+    var monitorProgress = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_episode)
-        hideSystemBars()
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
 
         videoView = findViewById(R.id.anime_video_view)
         videoView.setVideoURI(videoUrl)
@@ -38,26 +40,44 @@ class PlayEpisodeActivity : AppCompatActivity() {
         val mediaController = MediaController(this).apply {
             setAnchorView(videoView)
         }
+
         videoView.setMediaController(mediaController)
         videoView.setOnClickListener { hideSystemBars() }
         videoView.start()
 
+        findViewById<ConstraintLayout>(R.id.main).setOnClickListener {
+            showSystemBars()
+        }
+
         monitorVideoProgress()
     }
 
-    private fun hideSystemBars() {
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+    private fun showSystemBars() {
         WindowInsetsControllerCompat(window, window.decorView).apply {
-            hide(WindowInsetsCompat.Type.systemBars())
-            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_DEFAULT
+            show(WindowInsetsCompat.Type.statusBars())
+            show(WindowInsetsCompat.Type.displayCutout())
+            show(WindowInsetsCompat.Type.navigationBars())
+            CoroutineScope(Dispatchers.IO).launch {
+                delay(1000)
+                withContext(Dispatchers.Main) {
+                    hideSystemBars()
+                }
+            }
+        }
+    }
+
+    private fun hideSystemBars() {
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            hide(WindowInsetsCompat.Type.statusBars())
+            hide(WindowInsetsCompat.Type.displayCutout())
+            hide(WindowInsetsCompat.Type.navigationBars())
         }
     }
 
     private fun monitorVideoProgress() {
         CoroutineScope(Dispatchers.IO).launch {
-            while (true) {
-                delay(100)
+            while (monitorProgress) {
+                delay(1000)
                 if (videoView.duration > 0 && videoView.currentPosition.toFloat() / videoView.duration >= 0.75) {
                     val anilistID = AllAnimeParser().anilistWithAllAnimeID(allAnimeID)
                     val saved = AnilistParser().saveAnime(
@@ -67,10 +87,11 @@ class PlayEpisodeActivity : AppCompatActivity() {
                         baseContext
                     )
                     withContext(Dispatchers.Main) {
-                        if (saved){
+                        if (saved) {
                             Toast.makeText(baseContext, "Saved!", Toast.LENGTH_SHORT).show()
-                        }else{
-                            Toast.makeText(baseContext, "Failed to save!", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(baseContext, "Failed to save!", Toast.LENGTH_SHORT)
+                                .show()
                         }
                     }
                     break
