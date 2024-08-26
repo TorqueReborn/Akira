bb5a05f38f8104f02bc7c97e96489353997f21d7
TorqueReborn <torquereborn@gmail.com>
2024-08-27 00:46:46 +0530

    Intermediate layout

--- app/src/main/java/com/ghostreborn/akira/fragments/LoginFragment.kt
+++ app/src/main/java/com/ghostreborn/akira/fragments/LoginFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.ghostreborn.akira.R
 
@@ -13,5 +16,18 @@ class LoginFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_login, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val loginButton = view.findViewById<ImageView>(R.id.login_button)
+        val frame = view.findViewById<FrameLayout>(R.id.login_intermediate_frame)
+        val constraint = view.findViewById<ConstraintLayout>(R.id.login_constraint)
+
+        loginButton.setOnClickListener {
+            constraint.removeAllViews()
+            LayoutInflater.from(requireContext()).inflate(R.layout.fragment_login_intermediate, frame, true)
+        }
+    }
 
 }
