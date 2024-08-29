16735142d5fb097f25c7cc3c13c314ef90de9ffe
TorqueReborn <torquereborn@gmail.com>
2024-08-30 01:56:01 +0530

    Search UI with keyboard not cover editText in MainActivity

--- app/src/main/java/com/ghostreborn/akira/fragments/HomeFragment.kt
+++ app/src/main/java/com/ghostreborn/akira/fragments/HomeFragment.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ghostreborn.akira.R
-import com.ghostreborn.akira.anilist.AnilistParser
-import com.ghostreborn.akira.utils.AkiraUtils
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -24,12 +22,8 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         CoroutineScope(Dispatchers.IO).launch {
             withContext(Dispatchers.Main) {
-                val userID = AkiraUtils().getUserID(requireContext())
-                val anime = AnilistParser().userAnime(userID)
-                val manga = AnilistParser().userManga(userID)
                 childFragmentManager.beginTransaction()
-                    .replace(R.id.anime_frame, CommonFragment("Anime", anime))
-                    .replace(R.id.manga_frame, CommonFragment("Manga", manga, true))
+                    .replace(R.id.anime_frame, SearchFragment())
                     .commit()
             }
         }
---
+++ app/src/main/java/com/ghostreborn/akira/fragments/SearchFragment.kt
@@ -0,0 +1,16 @@
+package com.ghostreborn.akira.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ghostreborn.akira.R
+
+class SearchFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_search, container, false)
+}
