dee9658589c8e1da964ce2388fac54beae5fc73f
TorqueReborn <torquereborn@gmail.com>
2024-08-30 05:34:30 +0530

    Handle go button

--- app/src/main/java/com/ghostreborn/akira/fragments/SearchFragment.kt
+++ app/src/main/java/com/ghostreborn/akira/fragments/SearchFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.ghostreborn.akira.R
 
@@ -13,4 +16,19 @@ class SearchFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_search, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val search: EditText = view.findViewById(R.id.search_edit_text)
+        search.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_GO) {
+                Toast.makeText(requireContext(), "Clicked Go Button", Toast.LENGTH_SHORT).show()
+                true
+            }else{
+                false
+            }
+        }
+
+    }
 }
