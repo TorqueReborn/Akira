72a833af0b5eb1410656843c84b5d13052248566
TorqueReborn <torquereborn@gmail.com>
2024-09-05 17:33:17 +0530

    Find views and template code

--- app/src/main/java/com/ghostreborn/akira/fragment/LoginFragment.kt
+++ app/src/main/java/com/ghostreborn/akira/fragment/LoginFragment.kt
@@ -4,13 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.ghostreborn.akira.R
 
 class LoginFragment : Fragment() {
+
+    lateinit var userNameEdit: EditText
+    lateinit var userPassEdit: EditText
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_login, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        userNameEdit = view.findViewById(R.id.login_user_edit)
+        userPassEdit = view.findViewById(R.id.login_pass_edit)
+        view.findViewById<Button>(R.id.login_button).setOnClickListener {
+            checkLogin()
+        }
+    }
+
+    private fun checkLogin(){
+        val userName = userNameEdit.text.toString()
+        val userPass = userPassEdit.text.toString()
+        if (userName.isNotEmpty() && userPass.isNotEmpty()){
+            Toast.makeText(requireContext(), "Logged In", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
