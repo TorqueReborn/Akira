73f320c5a87658cc2508905ffbd38ba915e81509
TorqueReborn <torquereborn@gmail.com>
2024-09-07 13:11:22 +0530

    Show anime list

--- app/src/main/java/com/ghostreborn/akira/MainActivity.kt
+++ app/src/main/java/com/ghostreborn/akira/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ghostreborn.akira
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ghostreborn.akira.fragment.HomeFragment
 import com.ghostreborn.akira.fragment.LoginFragment
 import com.ghostreborn.akira.utils.AkiraUtils
 
@@ -14,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         if (!AkiraUtils().checkLogin(this)) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.main_frame, LoginFragment())
+                .commit()
+        }else{
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frame, HomeFragment())
                 .commit()
         }
 
---
+++ app/src/main/java/com/ghostreborn/akira/fragment/HomeFragment.kt
@@ -0,0 +1,50 @@
+package com.ghostreborn.akira.fragment
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ghostreborn.akira.R
+import com.ghostreborn.akira.adapter.AnimeAdapter
+import com.ghostreborn.akira.models.Anime
+import com.ghostreborn.akira.utils.AkiraUtils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_home, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.home_recycler)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val db = AkiraUtils().getDB(requireContext())
+            val list = db.savedEntryDao().getAll()
+            val anime = ArrayList<Anime>()
+            for (entry in list) {
+                anime.add(Anime(entry.kitsuID, entry.anime, entry.thumbnail))
+            }
+
+            withContext(Dispatchers.Main) {
+                val adapter = AnimeAdapter(anime)
+                recyclerView.adapter = adapter
+                recyclerView.layoutManager = LinearLayoutManager(requireContext())
+            }
+        }
+
+
+    }
+}
