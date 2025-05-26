5df3c5cdd360ecede5b2a03385506e6e63818d0d
TorqueReborn <torquereborn@gmail.com>
2025-05-26 19:26:57 +0530

    Drop Everything (You know me)

--- app/src/main/java/com/ghostreborn/akira/MainActivity.kt
+++ app/src/main/java/com/ghostreborn/akira/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.ghostreborn.akira
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.ghostreborn.akira.fragment.HomeFragment
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.main_frame, HomeFragment())
-            .commit()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
     }
 }
