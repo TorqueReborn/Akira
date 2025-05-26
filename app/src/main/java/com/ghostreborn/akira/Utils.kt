435994474c9b4f1cef34a14a134c31566b1b0b2d
TorqueReborn <torquereborn@gmail.com>
2025-05-27 03:24:40 +0530

    Improve season calculation
    The `calculateQuarter` function now correctly determines the season based on the current month.
    The logic for `getMonth` has been updated to accurately map months to their respective seasons:
    - Winter: December, January, February
    - Spring: March, April, May
    - Summer: June, July, August
    - Fall: September, October, November

--- app/src/main/java/com/ghostreborn/akira/Utils.kt
+++ app/src/main/java/com/ghostreborn/akira/Utils.kt
@@ -6,19 +6,21 @@ class Utils {
 
     fun calculateQuarter(dec: Int): Pair<String, String> {
         val currentDate = Calendar.getInstance()
-        currentDate.add(Calendar.MONTH, -(3 * (dec + 1)))
+        currentDate.add(Calendar.MONTH, -(3 * dec))
 
         val month = currentDate.get(Calendar.MONTH)
         val year = currentDate.get(Calendar.YEAR)
-        val parsedMonth = getMonth(month % 4)
+
+        val parsedMonth = getMonth(month)
         return Pair(parsedMonth, "$year")
     }
 
     private fun getMonth(month: Int): String {
         return when (month) {
-            1 -> "Spring"
-            2 -> "Summer"
-            3 -> "Fall"
+            1, 2, 12 -> "Winter"
+            3, 4, 5 -> "Spring"
+            6, 7, 8 -> "Summer"
+            9, 10, 11 -> "Fall"
             else -> "Winter"
         }
     }
