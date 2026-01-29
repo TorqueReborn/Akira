c4412e66f761ad7c8aa9cdd693a7f9ea336fbffa
TorqueReborn <torquereborn@gmail.com>
2026-01-29 14:14:08 +0530

    Target SDK for Android 16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.ghostreborn.akira"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.ghostreborn.akira"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
