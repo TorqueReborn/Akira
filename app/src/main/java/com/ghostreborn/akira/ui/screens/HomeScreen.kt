1dd2c019b817ab0dd00b19885982ac12dde1c9d2
TorqueReborn <torquereborn@gmail.com>
2026-01-29 23:28:09 +0530

    Show template animes

--- app/src/main/java/com/ghostreborn/akira/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/ghostreborn/akira/ui/screens/HomeScreen.kt
@@ -1,15 +1,25 @@
 package com.ghostreborn.akira.ui.screens
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.Alignment
-import androidx.compose.material3.Text
+import com.ghostreborn.akira.utils.Utils
 import androidx.compose.runtime.Composable
-import androidx.compose.foundation.layout.Box
+import com.ghostreborn.akira.ui.components.AnimeCard
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 
 @Composable
 fun HomeScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Home Screen")
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 128.dp),
+        contentPadding = PaddingValues(8.dp),
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val anime = Utils().getSampleAnimeList()
+        items(anime.size) { index ->
+            AnimeCard(anime = anime[index])
+        }
     }
 }
