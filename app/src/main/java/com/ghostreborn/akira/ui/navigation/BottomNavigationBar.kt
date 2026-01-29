afb7e62a8f1e03ea1d805ad1a516431abe13f3f4
TorqueReborn <torquereborn@gmail.com>
2026-01-29 21:34:57 +0530

    Float navigation

--- app/src/main/java/com/ghostreborn/akira/ui/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/ghostreborn/akira/ui/navigation/BottomNavigationBar.kt
@@ -1,11 +1,16 @@
 package com.ghostreborn.akira.ui.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 
@@ -17,7 +22,11 @@ fun BottomNavigationBar(navController: NavController) {
         BottomNavItem.Settings
     )
 
-    NavigationBar {
+    NavigationBar(
+        modifier = Modifier
+            .padding(16.dp)
+            .clip(RoundedCornerShape(16.dp))
+    ) {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
 
