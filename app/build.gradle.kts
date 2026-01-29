d45f3869b214a16fbc256b186a79508b0d5c328e
TorqueReborn <torquereborn@gmail.com>
2026-01-29 20:25:40 +0530

    Bottom Navigation Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
