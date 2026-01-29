489c4936e020a1529a7a46597b0fb8028600f438
TorqueReborn <torquereborn@gmail.com>
2026-01-29 20:35:44 +0530

    data object for Home, Profile, Setting

--- app/src/main/java/com/ghostreborn/akira/ui/navigation/BottomNavItem.kt
+++ app/src/main/java/com/ghostreborn/akira/ui/navigation/BottomNavItem.kt
@@ -1,6 +1,13 @@
 package com.ghostreborn.akira.ui.navigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class BottomNavItem(val route: String, val icon: ImageVector, val label: String) {
+    data object Home : BottomNavItem("home", Icons.Default.Home, "Home")
+    data object Profile : BottomNavItem("profile", Icons.Default.Person, "Profile")
+    data object Settings : BottomNavItem("settings", Icons.Default.Settings, "Settings")
 }
